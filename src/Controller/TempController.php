d43bdaf9ffcd5ac3975500c93e8dfea9f698a0cb
Jimmy Metz <35279188+jrm402@users.noreply.github.com>
2021-03-27 03:48:05 -0600

    Handle form submit

--- src/Controller/TempController.php
+++ src/Controller/TempController.php
@@ -10,17 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 // Form builder
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TempController extends AbstractController
 {
+
+	private Request $request;
 
     /**
      * @Route("/temp", name="temp")
      */
     public function index(Request $request): Response
     {
-
 		/**
 		 * DBAL
 		 */
@@ -41,58 +43,76 @@ class TempController extends AbstractController
 
 
 		/**
-		 * Build a form.
+		 * Build a form and handle submit.
 		 */
+		$this->request = $request;
+		$form = $this->buildForm();
+		if($this->validateForm($form)) {
+			return $this->submitForm($form);
+		}
+
+		/**
+		 * Return the content.
+		 */
+        return $this->render('temp/index.html.twig', [
+			'form' => $form->createView(),
+        ]);
+    }
+
+	/**
+	 * Create a form.
+	 */
+	private function buildForm(): FormInterface
+	{
 		$fb = $this->createFormBuilder();
+
 		$fb->add('task', TextType::class, [
 			'label' => 'Task Title',
 			'required' => false,
 			'attr' => [
 				'placeholder' => 'Task title',
 				'class' => 'hello-dolly',
-			]
-			// 'constraints' => [
-			// 	new NotBlank(),
-			// ],
+			],
 		]);
 		$fb->add('save', SubmitType::class, [
 			'label' => 'Create Task',
 		]);
-		$form = $fb->getForm();
 
+		return $fb->getForm();
+	}
+
+	/**
+	 * Validate form.
+	 */
+	private function validateForm(FormInterface $form): bool
+	{
 		// Handle the form request.
-		$form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
+		$form->handleRequest($this->request);
+		if($form->isSubmitted() && $form->isValid()) {
+			$data = $form->getData();
+			$success = true;
 
-			$this->addFlash(
-	            'success',
-	            'Success Message!'
-	        );
-			$this->addFlash(
-	            'primary',
-	            'Primary Message!'
-	        );
-			$this->addFlash(
-	            'primary',
-	            'Primary Message!'
-	        );
-			$this->addFlash(
-	            'danger',
-	            'Danger Message!'
-	        );
+			if($data['task'] == '') {
+				$this->addFlash('danger', 'Task cannot be blank.');
+				$success = false;
+			}
 
-			echo '<pre>'.print_r($data, true).'</pre>';
-            // return $this->redirectToRoute('task_success');
+			return $success;
+
+			// $this->addFlash('success', 'Success Message! @temp');
+			// $this->addFlash('primary', 'Primary Message!');
+			// $this->addFlash('danger', 'Danger Message!');
         }
 
+		return false;
+	}
 
-		/**
-		 * Return the content.
-		 */
-        return $this->render('temp/index.html.twig', [
-            'controller_name' => 'TempController',
-			'form' => $form->createView(),
-        ]);
-    }
+	/**
+	 * Submit form.
+	 */
+	private function submitForm(FormInterface $form): Response
+	{
+		$this->addFlash('success', 'Form submitted successfully');
+		return $this->redirectToRoute('temp');
+	}
 }
